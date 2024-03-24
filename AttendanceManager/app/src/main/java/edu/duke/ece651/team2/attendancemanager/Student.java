443b5b4092da0266f74cf29e6acc4a7d088a233d
Kenan Colak <kc566@duke.edu>
2024-03-24 12:18:12 -0400

    added text UI to Student.java

--- AttendanceManager/app/src/main/java/edu/duke/ece651/team2/attendancemanager/Student.java
+++ AttendanceManager/app/src/main/java/edu/duke/ece651/team2/attendancemanager/Student.java
@@ -7,10 +7,10 @@ public class Student{
   String displayName;
 
   public Student(){
-    this.legalName = new String();
-    this.studentID = new String();
-    this.email = new String();
-    this.displayName = new String();
+    this.legalName = "";
+    this.studentID = "";
+    this.email = "";
+    this.displayName = "";
   }
 
   public Student(String ln, String si, String e, String dn){
@@ -21,24 +21,38 @@ public class Student{
   }
 
   void setLegalName(String legalName){
+    System.out.println("Please enter Student's legal name: ");
     this.legalName = legalName;
   }
 
   String getLegalName(){return legalName;}
 
   void setStudentID(String studentID){
+    System.out.println("Please enter Student's ID: ");
     this.studentID = studentID;
   }
 
   String getStudentID(){return studentID;}
   void setEmail(String email){
+    System.out.println("Please enter Student's email address: ");
     this.email = email;
   }
 
   String getEmail(){return email;}
   
   void setDisplayName(String displayName){
+    System.out.println("Please enter Student's display name: ");
     this.displayName = displayName;
   }
   String getDisplayName(){return displayName;}
+
+  void addCheck(){
+    if(legalName != "" && studentID != "" &&
+       email != "" && displayName != ""){
+      System.out.println("Student " + legalName + "Successfully Added");
+    }
+    else{
+      System.out.println("Failed to add all fields for the student");
+    }
+  }
 }
