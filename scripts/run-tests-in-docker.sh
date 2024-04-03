dcfd1b3d3b1545447df415f70450019c5464ffb5
Charles Prior <charlie@charlieprior.com>
2024-04-03 19:28:24 -0400

    Added --network=host to docker run commands

--- scripts/run-tests-in-docker.sh
+++ scripts/run-tests-in-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir coverage
 chmod 777 coverage
-docker run --rm -v `pwd`/coverage:/coverage-out  citest scripts/test.sh
+docker run --network=host --rm -v `pwd`/coverage:/coverage-out  citest scripts/test.sh
