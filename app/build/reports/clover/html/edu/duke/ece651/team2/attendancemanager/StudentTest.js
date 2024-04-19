fb5f3d4be4145328f54229c961af02a606544e1a
LouiseLi777 <xl435@duke.edu>
2024-04-18 21:11:06 -0400

    javafx setup in general

---
+++ app/build/reports/clover/html/edu/duke/ece651/team2/attendancemanager/StudentTest.js
@@ -0,0 +1,10 @@
+var clover = new Object();
+
+// JSON: {classes : [{name, id, sl, el,  methods : [{sl, el}, ...]}, ...]}
+clover.pageData = {"classes":[{"el":21,"id":1010,"methods":[{"el":20,"sc":3,"sl":8}],"name":"StudentTest","sl":7}]}
+
+// JSON: {test_ID : {"methods": [ID1, ID2, ID3...], "name" : "testXXX() void"}, ...};
+clover.testTargets = {"test_19":{"methods":[{"sl":8}],"name":"test_setter_getter","pass":true,"statements":[{"sl":10},{"sl":11},{"sl":12},{"sl":13},{"sl":14},{"sl":15},{"sl":16},{"sl":17},{"sl":18},{"sl":19}]}}
+
+// JSON: { lines : [{tests : [testid1, testid2, testid3, ...]}, ...]};
+clover.srcFileLines = [[], [], [], [], [], [], [], [], [19], [], [19], [19], [19], [19], [19], [19], [19], [19], [19], [19], [], []]
