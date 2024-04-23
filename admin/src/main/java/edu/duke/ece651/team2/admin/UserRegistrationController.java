e192cf5d177581b2919cb5ee76c2a2506cb3d0b3
Kenan Colak <kc566@duke.edu>
2024-04-23 17:28:33 -0400

    Student Update Testing 2

--- admin/src/main/java/edu/duke/ece651/team2/admin/UserRegistrationController.java
+++ admin/src/main/java/edu/duke/ece651/team2/admin/UserRegistrationController.java
@@ -93,8 +93,8 @@ public class UserRegistrationController {
         int res = controller.addStudentController(credentials);
         //Object source = event.getSource();
         if(res != 0 ||
-                !Objects.equals(credentials[0], "") || !Objects.equals(credentials[1], "") ||
-                !Objects.equals(credentials[2], "") || !Objects.equals(credentials[3], "")){
+                (!Objects.equals(credentials[0], "") && !Objects.equals(credentials[1], "") &&
+                !Objects.equals(credentials[2], "") && !Objects.equals(credentials[3], ""))){
             showAlert("Sign-Up Successful!");
         }
         else{
@@ -149,8 +149,8 @@ public class UserRegistrationController {
         credentials[0] = studentIDNumber.getText();
         credentials[1] = studentPassword.getText();
         int res = controller.updateStudentController(credentials);
-        if(res != 0 || !Objects.equals(credentials[0], "")
-                    || !Objects.equals(credentials[1], "")){
+        if(res != 0 || (!Objects.equals(credentials[0], "")
+                    && !Objects.equals(credentials[1], ""))){
             showAlert("Update Successful!");
         }
         else{
