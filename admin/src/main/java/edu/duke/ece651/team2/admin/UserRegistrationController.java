7374c2d3fc46782ab852f6742c48ed403d65d175
Kenan Colak <kc566@duke.edu>
2024-04-22 23:46:12 -0400

    Debugging

--- admin/src/main/java/edu/duke/ece651/team2/admin/UserRegistrationController.java
+++ admin/src/main/java/edu/duke/ece651/team2/admin/UserRegistrationController.java
@@ -38,7 +38,7 @@ public class UserRegistrationController {
         Button b = (Button) event.getSource();
         Stage stage = (Stage) b.getScene().getWindow();
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/StudentOptions.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/UserSelect.fxml"));
             TitledPane page = (TitledPane) loader.load();
             Scene newScene = new Scene(page);
             stage.setScene(newScene);
@@ -52,7 +52,7 @@ public class UserRegistrationController {
         Button b = (Button) event.getSource();
         Stage stage = (Stage) b.getScene().getWindow();
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/FacultyOptions.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/UserSelect.fxml"));
             TitledPane page = (TitledPane) loader.load();
             Scene newScene = new Scene(page);
             stage.setScene(newScene);
