d4a311a5ce3f9efb7d7276b2d0b3e6e00f387575
Kenan Colak <kc566@duke.edu>
2024-04-24 20:35:34 -0400

    Fixing issues

--- admin/src/main/java/edu/duke/ece651/team2/admin/UserRegistrationController.java
+++ admin/src/main/java/edu/duke/ece651/team2/admin/UserRegistrationController.java
@@ -298,7 +298,7 @@ public class UserRegistrationController {
                     && !Objects.equals(credentials[2], ""))){
             showAlert("Update Successful: \n" + "Display Name and Password have been changed!");
         }
-        if(res == 2 && (!Objects.equals(credentials[0], "")
+        else if(res == 2 && (!Objects.equals(credentials[0], "")
                 && !Objects.equals(credentials[1], "")
                 && !Objects.equals(credentials[2], ""))){
             showAlert("Update Successful: \n" + "Password has been changed!\n" + "(Display Name Access has Been Limited by Admin)");
