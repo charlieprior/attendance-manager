419cfe49f221cb02c6f13489f6c5e5f07432fdef
Charles Prior <charlie@charlieprior.com>
2024-04-25 22:18:10 -0400

    Bulk add students finished untested

--- courseManagement/src/main/java/edu/duke/ece651/team2/courseManagement/BulkAddStudentsController.java
+++ courseManagement/src/main/java/edu/duke/ece651/team2/courseManagement/BulkAddStudentsController.java
@@ -1,19 +1,22 @@
 package edu.duke.ece651.team2.courseManagement;
 
+import edu.duke.ece651.team2.shared.Section;
+import edu.duke.ece651.team2.shared.Student;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BulkAddStudentsController extends UpdateSectionSuperController {
     @FXML Button ChooseFileButton;
     @FXML Text FilenameText;
+    @FXML CheckBox HeaderCheckBox;
     @FXML TextField DelimiterField;
     @FXML TextField LegalNameIndexField;
     @FXML TextField DisplayNameIndexField;
@@ -22,26 +25,80 @@ public class BulkAddStudentsController extends UpdateSectionSuperController {
 
     FileChooser fileChooser;
 
+    File selectedFile;
+
     BulkAddStudentsController(CourseManagement model) {
         super(model, "/views/bulkAddStudents.fxml");
 
-//        setFileChooser();
-//        setChooseFileButton();
-    }
-
-    private void setFileChooser() {
-        fileChooser.setTitle("Open Resource File");
-        fileChooser.getExtensionFilters().addAll(
-                new ExtensionFilter("Text Files", "*.txt"),
-                new ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"),
-                new ExtensionFilter("Audio Files", "*.wav", "*.mp3", "*.aac"),
-                new ExtensionFilter("All Files", "*.*"));
+        fileChooser = new FileChooser();
+        setChooseFileButton();
+        setAddButton();
     }
 
     private void setChooseFileButton() {
         ChooseFileButton.setOnAction(actionEvent -> {
-            File selectedFile = fileChooser.showOpenDialog(ChooseFileButton.getScene().getWindow());
+            selectedFile = fileChooser.showOpenDialog(ChooseFileButton.getScene().getWindow());
             FilenameText.setText(selectedFile.getName());
+        });
+    }
+
+    private void setAddButton() {
+        AddButton.setOnAction(actionEvent -> {
+            Section section = SectionSelector.getValue();
+            if (section == null) {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Please select a section", ButtonType.OK);
+                alert.showAndWait();
+                return;
+            }
+
+            try {
+                Integer legalNameIndex = Integer.valueOf(LegalNameIndexField.getText());
+                Integer displayNameIndex = Integer.valueOf(DisplayNameIndexField.getText());
+                Integer emailIndex = Integer.valueOf(EmailIndexField.getText());
+
+                if (legalNameIndex.equals(displayNameIndex) ||
+                        legalNameIndex.equals(emailIndex) ||
+                        displayNameIndex.equals(emailIndex)) {
+                    Alert alert = new Alert(Alert.AlertType.ERROR, "Indices must be unique", ButtonType.OK);
+                    alert.showAndWait();
+                    return;
+                }
+
+                String delimiter = DelimiterField.getText();
+
+                if (delimiter.isEmpty()) {
+                    Alert alert = new Alert(Alert.AlertType.ERROR, "Please enter a delimiter", ButtonType.OK);
+                    alert.showAndWait();
+                    return;
+                }
+
+                if (selectedFile == null) {
+                    Alert alert = new Alert(Alert.AlertType.ERROR, "Please select a file", ButtonType.OK);
+                    alert.showAndWait();
+                    return;
+                }
+
+                CSVLoader csvLoader = new CSVLoader();
+
+                try {
+                    List<String> lines = csvLoader.getLines(selectedFile.getAbsolutePath(), HeaderCheckBox.isSelected());
+                    List<Student> students = csvLoader.getStudents(lines, delimiter, legalNameIndex, emailIndex, displayNameIndex, model.getUniversity().getId());
+                    for (Student student : students) {
+                        model.addStudentToSection(student, section);
+                    }
+                } catch (IOException e) {
+                    Alert alert = new Alert(Alert.AlertType.ERROR, "File could not be read", ButtonType.OK);
+                    alert.showAndWait();
+                    AddButton.getScene().setRoot(model.getMainMenuController());
+                    return;
+                }
+
+            } catch (NumberFormatException e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Indices are required to be numbers", ButtonType.OK);
+                alert.showAndWait();
+                return;
+            }
+            AddButton.getScene().setRoot(model.getMainMenuController());
         });
     }
 }
