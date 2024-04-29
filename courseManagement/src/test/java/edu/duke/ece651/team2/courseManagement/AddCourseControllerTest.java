62c8ffde0f94cc026d9239098c06fa0169fd534a
Charles Prior <charlie@charlieprior.com>
2024-04-29 13:17:03 -0400

    Test empty name working

--- courseManagement/src/test/java/edu/duke/ece651/team2/courseManagement/AddCourseControllerTest.java
+++ courseManagement/src/test/java/edu/duke/ece651/team2/courseManagement/AddCourseControllerTest.java
@@ -12,7 +12,9 @@ import javafx.stage.Stage;
 import javafx.stage.Window;
 import org.checkerframework.checker.units.qual.C;
 import org.junit.jupiter.api.Test;
+import org.testfx.api.FxAssert;
 import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.matcher.base.WindowMatchers;
 import org.testfx.util.WaitForAsyncUtils;
 
 import java.util.HashSet;
@@ -48,42 +50,9 @@ class AddCourseControllerTest extends ApplicationTest {
         assertEquals(expected, new HashSet<>(model.listCourses().stream().map(Course::getName).collect(Collectors.toSet())));
     }
 
-//    @Test
-//    public void testEmptyName() {
-//        Platform.runLater(() -> {
-//            clickOn("#ConfirmButton");
-//
-//            interact(() -> {
-//
-//                ButtonBar buttonBar = lookup(".button-bar").query();
-//                Button okButton = (Button) buttonBar.getButtons().stream()
-//                        .filter(button -> button instanceof Button && "OK".equals(((Button) button).getText()))
-//                        .findFirst()
-//                        .orElseThrow(() -> new RuntimeException("OK button not found"));
-//                clickOn(okButton);
-//            });
-//        });
-//    }
-
-    // Utility method to print all nodes
-    private void printNodes(Node node, String indent) {
-        System.out.println(indent + node.getClass().getSimpleName() + ": " + node);
-        if (node instanceof Parent) {
-            Parent parent = (Parent) node;
-            for (Node child : parent.getChildrenUnmodifiable()) {
-                printNodes(child, indent + "  ");
-            }
-        }
-    }
-
     @Test
-    public void listAllWindows() {
-        List<Window> windows = Window.getWindows();
-        for (Window window : windows) {
-            if (window instanceof Stage) {
-                Stage stage = (Stage) window;
-                System.out.println("Stage Title: " + stage.getTitle() + ", Scene: " + stage.getScene());
-            }
-        }
+    public void testEmptyName() {
+        clickOn("#ConfirmButton");
+        FxAssert.verifyThat(window("Error"), WindowMatchers.isShowing());
     }
 }
