50b2ea94a4c89cf2a73e2c543c085cf706e0f717
Charles Prior <charlie@charlieprior.com>
2024-04-29 16:41:35 -0400

    AddSectionControllerTest finished

--- courseManagement/src/test/java/edu/duke/ece651/team2/courseManagement/AddSectionControllerTest.java
+++ courseManagement/src/test/java/edu/duke/ece651/team2/courseManagement/AddSectionControllerTest.java
@@ -9,7 +9,9 @@ import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
+import org.testfx.api.FxAssert;
 import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.matcher.base.WindowMatchers;
 import org.testfx.util.WaitForAsyncUtils;
 
 import java.util.HashSet;
@@ -50,6 +52,37 @@ class AddSectionControllerTest extends ApplicationTest {
             clickOn("#SectionNameField");
             write("SectionName");
             clickOn("#ConfirmButton");
+
+        Set<String> expected = new HashSet<>();
+        expected.add("SectionName");
+        assertEquals(expected, new HashSet<>(model.getSections(course).stream().map(Section::getName).collect(Collectors.toSet())));
+    }
+
+    @Test
+    public void testCourseNull() {
+        clickOn("#SectionNameField");
+        write("SectionName");
+        clickOn("#ConfirmButton");
+        FxAssert.verifyThat(window("Error"), WindowMatchers.isShowing());
+    }
+
+    @Test
+    public void testNameEmpty() {
+        clickOn("#CourseSelector");
+        type(KeyCode.DOWN);
+        type(KeyCode.ENTER);
+        clickOn("#ConfirmButton");
+        FxAssert.verifyThat(window("Error"), WindowMatchers.isShowing());
+    }
+
+    @Test
+    public void testNullProfessor() {
+        clickOn("#CourseSelector");
+        type(KeyCode.DOWN);
+        type(KeyCode.ENTER);
+        clickOn("#SectionNameField");
+        write("SectionName");
+        clickOn("#ConfirmButton");
 
         Set<String> expected = new HashSet<>();
         expected.add("SectionName");
